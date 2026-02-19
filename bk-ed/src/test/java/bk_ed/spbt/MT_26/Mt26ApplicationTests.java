1d7e0839416f906d602aead8039573bd88bb413e
vidheshparthiv <vidhesh2024@gmail.com>
2026-02-19 09:28:10 +0530

    chore: initialize spring-boot setup

---
+++ bk-ed/src/test/java/bk_ed/spbt/MT_26/Mt26ApplicationTests.java
@@ -0,0 +1,13 @@
+package bk_ed.spbt.MT_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mt26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
