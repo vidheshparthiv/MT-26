cb3bc40bad4556f445c388d911dff5fabc46e465
vidheshparthiv <vidhesh2024@gmail.com>
2026-02-22 08:54:34 +0530

    chore: initialize springboot structure

---
+++ bk-ed/src/main/java/bk_ed/spbt/MT_26/Mt26Application.java
@@ -0,0 +1,13 @@
+package bk_ed.spbt.MT_26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mt26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mt26Application.class, args);
+	}
+
+}
