fb587201a5b1d861438b16f16f83ad3b528cc2a3
vidheshparthiv <vidhesh2024@gmail.com>
2026-02-28 22:17:45 +0530

    Create AppUser.java

---
+++ bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/AppUser.java
@@ -0,0 +1,23 @@
+package bk_ed.spbt.MT_26.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Getters and Setters
+}
