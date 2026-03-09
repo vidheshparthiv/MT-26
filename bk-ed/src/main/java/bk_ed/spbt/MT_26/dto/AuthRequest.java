eaefa03f90116bbeda2e0cca1b79b3be7ae67c71
vidheshparthiv <vidhesh2024@gmail.com>
2026-03-09 18:59:09 +0530

    feat: add Authrequest class
    contains required attributes for authentication requests

--- bk-ed/src/main/java/bk_ed/spbt/MT_26/dto/AuthRequest.java
+++ bk-ed/src/main/java/bk_ed/spbt/MT_26/dto/AuthRequest.java
@@ -1,5 +1,29 @@
 package bk_ed.spbt.MT_26.dto;
 
 public class AuthRequest {
+    private String username;
+    private String password;
 
+    public AuthRequest() {}
+
+    public AuthRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
