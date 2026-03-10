dfbaefa99e2d9827de03a23115ed7e247cab1e43
vidheshparthiv <vidhesh2024@gmail.com>
2026-03-10 18:58:46 +0530

    feat: add User entity
    contains all attributes about the user and their authentication password

--- bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/AppUser.java
+++ bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/AppUser.java
@@ -19,5 +19,44 @@ public class AppUser {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public AppUser() {}
+
+    public AppUser(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
