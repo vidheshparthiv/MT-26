216618b84fd923b4998f96dc947352c89c7bc56f
vidheshparthiv <vidhesh2024@gmail.com>
2026-03-13 20:50:12 +0530

    add attributes

--- bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/Transaction.java
+++ bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/Transaction.java
@@ -1,5 +1,37 @@
 package bk_ed.spbt.MT_26.entity;
 
 public class Transaction {
-    
+    private String id;
+    private String type;
+    private double amount;
+
+    public Transaction(String id, String type, double amount) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
