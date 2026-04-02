fd8aa1e644f0d35bbbf32af9e650fad2747de842
vidheshparthiv <vidhesh2024@gmail.com>
2026-04-02 22:34:22 +0530

    Update Budget.java

--- bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/Budget.java
+++ bk-ed/src/main/java/bk_ed/spbt/MT_26/entity/Budget.java
@@ -51,27 +51,21 @@ public class Budget {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public AppUser getUser() {
         return user;
     }
-
     public void setUser(AppUser user) {
         this.user = user;
     }
-    
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-    
     public String getDescription() {
         return description;
     }
